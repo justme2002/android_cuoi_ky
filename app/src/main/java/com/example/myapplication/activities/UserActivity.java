86a5b0ad5deb947a50c38697af8802445077dc21
huy2002 <huy2002109@gmail.com>
2022-05-01 23:25:43 +0700

    first_push_to_git

---
+++ app/src/main/java/com/example/myapplication/activities/UserActivity.java
@@ -0,0 +1,101 @@
+package com.example.myapplication.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.myapplication.R;
+import com.example.myapplication.adapters.UserAdapter;
+import com.example.myapplication.databinding.ActivityUserBinding;
+import com.example.myapplication.lisnerers.UserListener;
+import com.example.myapplication.models.User;
+import com.example.myapplication.utilities.Constants;
+import com.example.myapplication.utilities.PreferenceManager;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserActivity extends AppCompatActivity implements UserListener {
+
+    private ActivityUserBinding binding;
+    private PreferenceManager preferenceManager;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityUserBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        getUsers();
+        setListeners();
+    }
+
+    private void setListeners () {
+        binding.imageBack.setOnClickListener(v -> onBackPressed());
+    }
+
+
+    private void getUsers () {
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECT_USERS)
+                .get()
+                .addOnCompleteListener(task -> {
+                    loading(false);
+                    String currentUserId = preferenceManager.getString(Constants.KEY_USER_ID);
+                    if (task.isSuccessful() && task.getResult() != null) {
+                        List<User> users = new ArrayList<>();
+                        for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()) {
+                            if (currentUserId.equals(queryDocumentSnapshot.getId())) {
+                                continue;
+                            } else {
+                                User user = new User();
+                                user.name = queryDocumentSnapshot.getString(Constants.KEY_NAME);
+                                user.email = queryDocumentSnapshot.getString(Constants.KEY_EMAIL);
+                                user.image = queryDocumentSnapshot.getString(Constants.KEY_IMAGE);
+                                user.token = queryDocumentSnapshot.getString(Constants.KEY_FCM_TOKEN);
+                                user.id = queryDocumentSnapshot.getId();
+                                users.add(user);
+                            }
+
+                            if (users.size() > 0) {
+                                UserAdapter userAdapter = new UserAdapter(users, this);
+                                binding.userRecyclerView.setAdapter(userAdapter);
+                                binding.userRecyclerView.setVisibility(View.VISIBLE);
+                            } else {
+                                showErrorMessage();
+                            }
+                        }
+                    } else {
+                        showErrorMessage();
+                    }
+                });
+    }
+
+    private void showErrorMessage () {
+        binding.textErrorMessage.setText(String.format("%s", "No user available"));
+        binding.textErrorMessage.setVisibility(View.VISIBLE);
+    }
+
+    private void loading (Boolean isLoading) {
+        if (isLoading) {
+            binding.progressBar.setVisibility(View.VISIBLE);
+        } else {
+            binding.progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override
+    public void onUserClicked(User user) {
+        Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
+        intent.putExtra(Constants.KEY_USER, user);
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/lisnerers/UserListener.java
@@ -0,0 +1,7 @@
+package com.example.myapplication.lisnerers;
+
+import com.example.myapplication.models.User;
+
+public interface UserListener {
+    void onUserClicked (User user);
+}
